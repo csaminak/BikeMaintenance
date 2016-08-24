8d6bd8553682a9eef06c5a700fec111a999ac8be
Samina Chhatwal <csaminak@gmail.com>
2016-08-24 14:21:59 -0400

    added some of the urls and their associated template for the general-info states

--- src/app/js/application/cyclist.module.js
+++ src/app/js/application/cyclist.module.js
@@ -8,38 +8,68 @@
 
     function cyclistConfig($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.when('', '/');
-        $urlRouterProvider.otherwise('/404');
+        $urlRouterProvider.otherwise('/not-found');
 
         $stateProvider
             .state('home', {
-
+                url: '/',
+                templateUrl: '/app/templates/general-info/home.template.html',
+                controller: '',
+                controllerAs: ''
             })
             .state('about', {
-
+                url: '',
+                templateUrl: '/app/templates/general-info/about-us.template.html',
+                controller: '',
+                controllerAs: ''
             })
             .state('profile', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('bike-stats', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('bike-form', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('ride-form', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('login', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('create-account', {
-
+                url: '',
+                templateUrl: '/app/templates/**',
+                controller: '',
+                controllerAs: ''
             })
             .state('contact', {
-
+                url: '',
+                templateUrl: '/app/templates/general-info/contact.template.html',
+                controller: '',
+                controllerAs: ''
             })
             .state('404', {
-
+                url: '/not-found',
+                templateUrl: '/app/templates/application/404.template.html',
+                controller: '',
+                controllerAs: ''
             });
 
     }
