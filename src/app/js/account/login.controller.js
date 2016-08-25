019125d5b9d7b9b1dbb28a20a25806725352c6c4
Samina Chhatwal <csaminak@gmail.com>
2016-08-25 13:09:47 -0400

    added controller file

---
+++ src/app/js/account/login.controller.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    angular.module('cyclist')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'maintenance'];
+
+    function LoginController($location, maintenance) {
+
+        this.login = authorizeCyclist;
+
+
+        function authorizeCyclist() {
+            $location.path('https://www.strava.com/oauth/authorize\
+                            ?client_id=13165\
+                            &response_type=code\
+                            &redirect_uri=localhost:3000\
+                            &scope=view_private\
+                            &state=loggedin\
+                            &approval_prompt=auto');
+            //Create a state for the path that is the redirect back
+            //when in that state I will automatically/on load look into the browser
+            //for a code and then do the rest of this function...
+            //send code to service.then redirect to user's profile with the information returned
+            var code = $location.path().split('code=')[1];
+            return code;
+        }
+
+    }
+
+
+    /* https://www.strava.com/oauth/authorize?
+    client_id=13165&response_type=code&redirect_uri=localhost:3000&
+    scope=view_private&state=loggedin&approval_prompt=auto */
+
+})();
