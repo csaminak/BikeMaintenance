ea0f40ab3e9476d5713dddecbc65fffecb802e34
Samina Chhatwal <csaminak@gmail.com>
2016-08-25 13:08:24 -0400

    created the service function

--- src/app/js/application/maintenance.service.js
+++ src/app/js/application/maintenance.service.js
@@ -1,6 +1,19 @@
 (function() {
     'use strict';
 
-    
+    angular.module('cyclist')
+        .factory('maintenance', MaintenanceService);
+
+    MaintenanceService.$inject = ['$http'];
+
+    function MaintenanceService($http) {
+
+        return {
+
+        };
+
+
+    }
+
 
 })();
