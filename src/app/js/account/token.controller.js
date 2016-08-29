60a3474cb9e9f47e12ab4d3911d95bc6ba53f164
Samina Chhatwal <csaminak@gmail.com>
2016-08-29 16:50:01 -0400

    able to send code to service function which will communicate with the backend, waiting until back-end is ready to accept code to then redirect user to their page and store their info

--- src/app/js/account/token.controller.js
+++ src/app/js/account/token.controller.js
@@ -4,14 +4,24 @@
     angular.module('cyclist')
         .controller('TokenController', TokenController);
 
-    TokenController.$inject = ['$location', 'maintenance'];
+    TokenController.$inject = ['maintenance'];
 
-    function TokenController($location, maintenance) {
+    function TokenController(maintenance) {
+        this.code = window.location.hash.split('code=')[1];
+        console.log(this.code); //TODO DELETE
 
-        maintenance.sendStravaCode($location.path().split('code=')[1]);
-        //change state in then to user to addBike;
+        maintenance.sendStravaCode(this.code)
+                .then(function(response) {
+                    console.log(response);
+                    //change state in then to user to addBike;
+                    //send code to service.then redirect to user's profile with
+                    //the information returned
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
 
-        //send code to service.then redirect to user's profile with the information returned
+
     }
 
 
