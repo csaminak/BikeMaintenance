9fd803a580e272af9ae95131a2f2436d87dd0f06
Samina Chhatwal <csaminak@gmail.com>
2016-08-30 14:17:16 -0400

    added tests for login fn in service

--- test/specs/maintenance.service.spec.js
+++ test/specs/maintenance.service.spec.js
@@ -13,6 +13,10 @@
             $httpBackend = _$httpBackend_;
 
             $httpBackend
+                .whenGET('/app/templates/general-info/home.template.html')
+                .respond('<h2>Home Page for Cycling App</h2>');
+
+            $httpBackend
                 .when('POST', 'https://cycling-app.herokuapp.com/oauth/strava', {
                     code: '32twehdtu8'
                 })
@@ -41,6 +45,8 @@
 
         }));
 
+
+        //TESTS
         test('maintenance service functions exist', function() {
             assert.isFunction(maintenance.login, 'login fn exists!');
             assert.isFunction(maintenance.addBike, 'login fn exists!');
@@ -48,9 +54,35 @@
             assert.isFunction(maintenance.getBikes, 'login fn exists!');
         });
 
+        test('login sends a code and gets back user data', function(done) {
+            var result = maintenance.login('32twehdtu8');
+            assert.isObject(result);
+            assert.isFunction(result.then);
+            assert.isFunction(result.catch);
+            result
+                .then(function(response) {
+                    assert.isObject(response.data);
+                    assert.strictEqual(response.data.token, '45etycfghvgyhiuop54678');
+                    assert.isObject(response.data.athlete);
+                    assert.isObject(response.data.client);
+                    done();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    assert.fail('should not be in catch if all info is submitting');
+                    done();
+                });
+            $httpBackend.flush();
+        });
+
+
+
+
+
+
+
+
+
     });
-
-
-
 
 })();
