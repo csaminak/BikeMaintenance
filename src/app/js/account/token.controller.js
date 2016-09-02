e3b3f094400dd088c21397daf9216d249848378c
Samina Chhatwal <csaminak@gmail.com>
2016-09-02 10:24:30 -0400

    added the expected response message if an error were to occur

--- src/app/js/account/token.controller.js
+++ src/app/js/account/token.controller.js
@@ -7,15 +7,24 @@
     TokenController.$inject = ['$state', 'maintenance'];
 
     function TokenController($state, maintenance) {
+        var that = this;
+        this.errorMsg = '';
         this.code = window.location.hash.split('code=')[1];
 
         maintenance.login(this.code)
                 .then(function(response) {
-                    console.log(response); //TODO anything else with response?
+                    console.log(response);
+                    if(response.status === 500) {
+                        that.errorMsg = 'Sorry, we seem to have some difficulty connecting\
+                                            to the site, please try again later?';
+                        return;
+                    }
                     $state.go('bike-form');
                 })
                 .catch(function(err) {
-                    console.log(err); //TODO what kind of error can occur?
+                    console.log(err);
+                    that.errorMsg = 'Sorry, we are unable to connect with Strava.';
+                    return;
                 });
 
 
