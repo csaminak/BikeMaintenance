f4708c3d3715cee06f5b9a3e9b0984dbde8245bf
Samina Chhatwal <csaminak@gmail.com>
2016-09-06 11:09:53 -0400

    removed comments and provide value that isn't being used

--- test/specs/login.controller.spec.js
+++ test/specs/login.controller.spec.js
@@ -6,23 +6,15 @@
     suite('login controller', function() {
 
         var loginCtrl;
-        var mockWindow = {};
 
         setup(module('cyclist'));
 
-        setup(module(function($provide) {
-            $provide.value('$window', mockWindow);
-        }));
-
         setup(inject(function($controller) {
             loginCtrl = $controller('LoginController');
-            mockWindow.location = 'some-site';
         }));
 
         test('login function redirects user', function() {
             assert.isFunction(loginCtrl.login, 'login is a fn');
-            // loginCtrl.login();
-            // assert.strictEqual(mockWindow.location, 'some-site');
         });
 
     });
